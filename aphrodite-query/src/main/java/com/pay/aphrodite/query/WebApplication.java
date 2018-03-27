e6ae9b4d4b8309ab51a4ec44be36c0419da68f9d
wangyangyang <baboshit@163.com>
2018-03-27 23:03:15 +0800

    first commit

---
+++ aphrodite-query/src/main/java/com/pay/aphrodite/query/WebApplication.java
@@ -0,0 +1,31 @@
+package com.pay.aphrodite.query;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication(scanBasePackages = { "com.pay" })
+@ServletComponentScan(basePackages = { "com.pay" })
+@EnableConfigurationProperties
+@PropertySource(ignoreResourceNotFound = true, value = {
+        "classpath:/application.properties",
+        "classpath:/system.properties"})
+@EnableAutoConfiguration
+public class WebApplication {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(WebApplication.class, args);
+        String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+        for (String profile : activeProfiles) {
+            logger.warn("Spring Boot use profile:{}", profile);
+        }
+    }
+
+}
