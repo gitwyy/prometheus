cbe089ead6a74bbed64a0c8a5315bc18dbe0dadf
wangyangyang <baboshit@hotmail.com>
2018-03-30 03:24:35 +0800

    muti datasource

--- aphrodite-query/src/main/java/com/pay/aphrodite/query/config/ds/HiveDataSource.java
+++ aphrodite-query/src/main/java/com/pay/aphrodite/query/config/ds/HiveDataSource.java
@@ -1,35 +1,62 @@
 package com.pay.aphrodite.query.config.ds;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
+@MapperScan(value = HiveDataSource.PACKAGE,basePackages = HiveDataSource.PACKAGE, sqlSessionFactoryRef = "hiveSqlSessionFactory")
 public class HiveDataSource {
 
-    @Autowired
-    private Environment env;
+    public static final String MAPPER_LOCATION = "classpath:com/pay/aphrodite/query/dao/hive/*.xml";
+    public static final String PACKAGE = "com.pay.aphrodite.query.dao.hive";
+
+    @Value("${hive.url}")
+    private String hiveUrl;
+    @Value("${hive.driverClassName}")
+    private String hiveDriverClassName;
+    @Value("${hive.username}")
+    private String hiveUsername;
+    @Value("${hive.password}")
+    private String hivePassword;
 
     @Bean(name = "hiveJdbcDataSource")
-    @Qualifier("hiveJdbcDataSource")
-    public DataSource dataSource() {
+    @Primary
+    public DataSource hiveDataSource() {
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(env.getProperty("hive.url"));
-        dataSource.setDriverClassName(env.getProperty("hive.driver-class-name"));
-        dataSource.setUsername(env.getProperty("hive.username"));
-        dataSource.setPassword(env.getProperty("hive.password"));
+        dataSource.setUrl(hiveUrl);
+        dataSource.setDriverClassName(hiveDriverClassName);
+        dataSource.setUsername(hiveUsername);
+        dataSource.setPassword(hivePassword);
         return dataSource;
     }
 
-    @Bean(name = "hiveJdbcTemplate")
-    public JdbcTemplate hiveJdbcTemplate(@Qualifier("hiveJdbcDataSource") DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+
+    @Bean(name = "hiveTransactionManager")
+    @Primary
+    public DataSourceTransactionManager hiveTransactionManager(@Qualifier("hiveJdbcDataSource")DataSource hiveDataSource) {
+        return new DataSourceTransactionManager(hiveDataSource);
+    }
+
+    @Bean(name = "hiveSqlSessionFactory")
+    @Primary
+    public SqlSessionFactory hiveSqlSessionFactory(@Qualifier("hiveJdbcDataSource") DataSource hiveDataSource)
+            throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(hiveDataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources(MAPPER_LOCATION));
+        return sessionFactory.getObject();
     }
 
 }
