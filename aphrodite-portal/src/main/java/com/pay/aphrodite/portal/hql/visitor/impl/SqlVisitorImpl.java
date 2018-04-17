689ddc90ad32e69d8be5902af4b384802261eb0f
wangyangyang <baboshit@hotmail.com>
2018-04-17 21:06:57 +0800

    实现SELECT 访问者

--- aphrodite-portal/src/main/java/com/pay/aphrodite/portal/hql/visitor/impl/SqlVisitorImpl.java
+++ aphrodite-portal/src/main/java/com/pay/aphrodite/portal/hql/visitor/impl/SqlVisitorImpl.java
@@ -1,10 +1,9 @@
 package com.pay.aphrodite.portal.hql.visitor.impl;
 
-import com.pay.aphrodite.portal.hql.compoment.SqlFromItem;
-import com.pay.aphrodite.portal.hql.compoment.SqlSelect;
-import com.pay.aphrodite.portal.hql.compoment.SqlSelectItem;
-import com.pay.aphrodite.portal.hql.compoment.SqlWhereItem;
+import com.pay.aphrodite.portal.hql.compoment.*;
 import com.pay.aphrodite.portal.hql.visitor.SqlVisitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.StringJoiner;
 import java.util.stream.Stream;
@@ -17,14 +16,52 @@ import java.util.stream.Stream;
  * @Description: sql 访问者
  **/
 public class SqlVisitorImpl implements SqlVisitor {
+    private Logger log = LoggerFactory.getLogger(SqlVisitorImpl.class);
     @Override
-    public String visit(SqlSelectItem sqlSelectItem) {
-        return null;
+    public String visit(final SqlSelectItem sqlSelectItem) {
+        log.debug("visit SqlSelectItem [{}]",sqlSelectItem);
+        StringBuilder sb = new StringBuilder();
+        switch (sqlSelectItem.getSqlObjectType()){
+            case EXPR:
+                log.debug("visit SqlSelectItem case [{}]",SqlObjectType.EXPR);
+                sb.append(sqlSelectItem.getOwner()).append(".").append(sqlSelectItem.getColumnName())
+                        .append(" as ").append(sqlSelectItem.getColumnAlias());
+                log.debug("visit SqlSelectItem case [{}] result [{}]",SqlObjectType.EXPR,sb.toString());
+                break;
+            case SELECT:
+                log.debug("visit SqlSelectItem case [{}]",SqlObjectType.SELECT);
+                sb.append("(").append(sqlSelectItem.getSqlObject().accept(this)).append(")")
+                        .append(" as ").append(sqlSelectItem.getColumnAlias());
+                log.debug("visit SqlSelectItem case [{}] result [{}]",SqlObjectType.SELECT,sb.toString());
+                break;
+            default:
+                log.debug("visit SqlSelectItem case DEFAULT  no case matched !");
+                break;
+        }
+        return sb.toString();
     }
 
     @Override
-    public String visit(SqlFromItem SqlFromItem) {
-        return null;
+    public String visit(SqlFromItem sqlFromItem) {
+        log.debug("visit SqlFromItem [{}]",sqlFromItem);
+        StringBuilder sb = new StringBuilder();
+        switch (sqlFromItem.getSqlObjectType()){
+            case TABLE_SOURCE_EXPR:
+                log.debug("visit SqlFromItem case [{}]",SqlObjectType.EXPR);
+                sb.append(sqlFromItem.getTableName()).append(" as ").append(sqlFromItem.getTableAlias());
+                log.debug("visit SqlFromItem case [{}] result [{}]",SqlObjectType.EXPR,sb.toString());
+                break;
+            case SELECT:
+                log.debug("visit SqlFromItem case [{}]",SqlObjectType.SELECT);
+                sb.append("(").append(sqlFromItem.getSqlObject().accept(this)).append(")")
+                        .append(" as ").append(sqlFromItem.getTableAlias());
+                log.debug("visit SqlFromItem case [{}] result [{}]",SqlObjectType.SELECT,sb.toString());
+                break;
+            default:
+                log.debug("visit SqlSelectItem case DEFAULT  no case matched !");
+                break;
+        }
+        return sb.toString();
     }
 
     @Override
@@ -34,18 +71,23 @@ public class SqlVisitorImpl implements SqlVisitor {
 
     @Override
     public String visit(SqlSelect sqlSelect) {
+
         String selectSql = sqlSelect.getSelectItemList().stream().map(select -> {
             return select.accept(this);
-        }).reduce("SELECT ", (x, y) -> {
-            return new StringJoiner(x).add(y).toString();
-        });
+        }).reduce((x, y) -> {
+            StringJoiner sj = new StringJoiner(", ");
+            return sj.add(x).add(y).toString();
+        }).get();
 
-        Stream<String> froms = sqlSelect.getFromItemList().stream().map(from -> {
+        sqlSelect.getFromItemList().stream().map(from -> {
             return from.accept(this);
+        }).reduce((x,y) ->{
+            StringJoiner sj = new StringJoiner(" ");
+            return sj.add(x).add(y).toString();
         });
 
 
-        Stream<String> wheres = sqlSelect.getWhereItemList().stream().map(where -> {
+        Stream<String> wheres = sqlSelect.getWhereItemList().stream().sorted((x,y) ->{return 1;}).map(where -> {
             return where.accept(this);
         });
 
